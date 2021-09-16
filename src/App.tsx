367c9a4c9482569406e27314998896f29f019e2f
kctrnn <kctrnn@gmail.com>
2021-09-16 14:26:02 +0700

    Setup authSaga with redux-saga

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useAppDispatch } from 'app/hooks';
+import { login, logout, signup } from 'features/auth/authSlice';
 import React from 'react';
 
 function App() {
-  return <div>APP</div>;
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <button onClick={() => dispatch(login({ email: '', password: '' }))}>
+        LOGIN
+      </button>
+
+      <button
+        onClick={() =>
+          dispatch(signup({ email: '', name: '', password: '', username: '' }))
+        }
+      >
+        REGISTER
+      </button>
+
+      <button onClick={() => dispatch(logout())}>LOGOUT</button>
+    </div>
+  );
 }
 
 export default App;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,20 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import authReducer from 'features/auth/authSlice';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
