c880974c79bb4879ce1be510f0a2feffcd9b40fe
Kim Chan <kctrnn@gmail.com>
2021-12-05 15:55:37 +0700

    Add solution share

--- src/features/solution/components/SolutionShare.tsx
+++ src/features/solution/components/SolutionShare.tsx
@@ -1,9 +1,14 @@
 import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { IconButton, Paper, Stack, Typography } from '@mui/material';
 import { Box } from '@mui/system';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useLocation } from 'react-router';
+
+const HOST = 'https://webpractice-c1se21.vercel.app';
 
 function SolutionShare() {
+  const location = useLocation();
+
   return (
     <Paper variant="outlined" sx={{ px: 2, py: 1 }}>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
@@ -14,13 +19,15 @@ function SolutionShare() {
             <TwitterIcon />
           </IconButton> */}
 
-          <IconButton>
+          {/* <IconButton>
             <LinkedInIcon />
-          </IconButton>
+          </IconButton> */}
 
-          <IconButton>
-            <ContentCopyRoundedIcon />
-          </IconButton>
+          <CopyToClipboard text={HOST.concat(location.pathname)}>
+            <IconButton>
+              <ContentCopyRoundedIcon />
+            </IconButton>
+          </CopyToClipboard>
         </Box>
       </Stack>
     </Paper>
